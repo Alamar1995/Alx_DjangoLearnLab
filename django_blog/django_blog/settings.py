2bd2befa8de6e111258c6a0f1ba04207df974a31
Nvawo Alamar Nvawo <nyawoalamar5@gmail.com>
2025-12-01 13:51:15 +0300

    Cleaned up DATABASES setting to remove unused USER/PORT keys for SQLite configuration.

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -118,7 +118,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # blog_project/settings.py
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
